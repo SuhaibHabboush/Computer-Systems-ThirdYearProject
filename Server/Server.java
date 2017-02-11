3f28b1e269d8097047f8f9d85e4dcf2865a62a2c
Brendan Lucas <brendanwflucas@gmail.com>
2017-02-11 17:14:51 -0500

    add server init method

--- Server/Server.java
+++ Server/Server.java
@@ -1,1 +1,30 @@
-got
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Server extends Thread{
+
+  private DatagramSocket receiveSocket;
+  private ArrayList<Thread> activeRequests;
+
+  private static final int PORT_NUMBER = 1400;
+
+  /*
+  * Creates new server Thread initalizes reveive socket and active requests
+  */
+  public Server(){
+      activeRequests = new ArrayList<Thread>();
+      try{
+        recieveSocket = new DatagramSocket(PORT_NUMBER);
+      } catch (SocketException e){
+        e.printStackTrace();
+      }
+  }
+
+  public void recieveAndControl(){
+    
+
+  }
+
+
+}
