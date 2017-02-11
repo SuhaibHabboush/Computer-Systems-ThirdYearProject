931388654ac6cee6206f3f313ff4a86835a0f2fb
Brendan Lucas <brendanwflucas@gmail.com>
2017-02-11 18:58:47 -0500

    complete server

--- Server/Server.java
+++ Server/Server.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class Server extends Thread{
-
+  private String passcode = "1472";
   private DatagramSocket receiveSocket;
   private ArrayList<Thread> activeRequests;
 
@@ -15,16 +15,33 @@ public class Server extends Thread{
   public Server(){
       activeRequests = new ArrayList<Thread>();
       try{
-        recieveSocket = new DatagramSocket(PORT_NUMBER);
+        receiveSocket = new DatagramSocket(PORT_NUMBER);
       } catch (SocketException e){
         e.printStackTrace();
       }
   }
 
   public void recieveAndControl(){
-    
+    byte[] msg = new byte[100];
+    DatagramPacket receivePacket = new DatagramPacket(msg, msg.length);
 
+    while(true){
+        System.out.println("Server waiting..\n");
+        try {
+          receiveSocket.receive(receivePacket);
+        } catch(IOException e){
+          e.printStackTrace();
+        }
+        System.out.println("Request Received");
+        addActiveRequest(new ControlThread(receivePacket));
+    }
   }
-
+  public void addActiveRequest(Thread request){
+    activeRequests.add(request);
+    request.start();
+  }
+  public String getPasscode(){
+    return passcode;
+  }
 
 }
