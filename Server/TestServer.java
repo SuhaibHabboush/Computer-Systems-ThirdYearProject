dfe6b0c655cdd6df4455bc79c2ced3d283ef5cd0
Brendan Lucas <brendanwflucas@gmail.com>
2017-02-24 13:33:35 -0500

    add testServer Class, add message opcode design text file

--- Server/TestServer.java
+++ Server/TestServer.java
@@ -3,13 +3,13 @@ import java.io.*;
 
 public class TestServer extends Thread{
   final int SERVER_PORT = 1400;
-
   final byte PASS_MSG = 0;
   final byte IMG_MSG = 1;
   final byte D_STAT_MSG = 2;
   final byte LK_MSG = 3;
   //TODO: add the rest of the oppcodes.
-  boolean passcodeSuccess;
+  boolean correctPasscodeSuccess;
+  boolean incorrectPasscodeSuccess;
   boolean imageSendSuccess;
   boolean invalidHomeSuccess;
   boolean invalidDoorSuccess;
@@ -17,15 +17,14 @@ public class TestServer extends Thread{
   boolean unlockDoorRequestRecievedSuccess;
   boolean unlockDoorRequestSentSuccess;
   //NOTE: Maybe have opcode for all doors in house for general requests from webclients to be 0000;
-  final String CORECT_PASS = "1324";
-  final String INCORECT_PASS = "1234";
+  final String CORRECT_PASS = "1324";
+  final String INCORRECT_PASS = "1234";
   byte[] sendMsg;
-
+  InetAddress LOCAL_HOST = InetAddress.getLocalHost();
   Server server;
 
   DatagramSocket doorSocket;
   DatagramSocket webclientSocket;
-  int SERVER_PORT = 1400;
 
   public TestServer() {
       this.server = new Server();
@@ -37,18 +36,17 @@ public class TestServer extends Thread{
   }
 
 
-  public void bulidStandardRequest(byte b, Object o){
+  public void bulidStandardRequest(byte b, String s) {
     sendMsg[0] = 1;
-    sndMsg[1] = ;
-    sndMsg[2] = b;
-    if (o == null) return;
-    else if (o.getType() == String.getType()){
-      byte[] tempBytes = o.getBytes();
-      int j=0;
-      for(int i = 3; i<tempBytes.length; i++){
-        sendMsg[i] = tempBytes[j]++;
-      }
+    sendMsg[1] = 1;
+    sendMsg[2] = b;
+    if (s == null) return;
+    byte[] tempBytes = s.getBytes();
+    int j=0;
+    for(int i = 3; i<tempBytes.length; i++){
+      sendMsg[i] = tempBytes[j++];
     }
+
   }
 
   public void run(){
@@ -57,16 +55,30 @@ public class TestServer extends Thread{
 
     System.out.println("Test Server recieve Message to unlock door from webclient");
 
-    buildStandardRequest(PASS_MSG, CORRECT_PASS);
-    DataPacket testPacket = new DatagramPacket(sendMsg, sendMsg.length, localHost, SERVER_PORT);
-
+    //Test Correct Passcode
+    bulidStandardRequest(PASS_MSG, CORRECT_PASS);
+    DatagramPacket testPacket = new DatagramPacket(sendMsg, sendMsg.length, LOCAL_HOST, SERVER_PORT);
     doorSocket.setSoTimeout(10000);
     doorSocket.send(testPacket);
+    //NOTE: server has to send to receiveMsg.getPort()
+    doorSocket.receive(recievePacket);
+    correctPasscodeSuccess = rcvMsg[3]==1;
+    System.out.println("Correct Passcode Test: ");
+    if(correctPasscodeSuccess) System.out.println("Success");
+    else System.out.println("Failed");
 
-    doorSocket.recieve(recievePacket);
-    for(int i =3)
-
-
+    //TEST incorect Passcode
+    bulidStandardRequest(PASS_MSG, INCORRECT_PASS);
+    testPacket = new DatagramPacket(sendMsg, sendMsg.length, LOCAL_HOST, SERVER_PORT);
+    rcvMsg = new byte[512];
+    doorSocket.setSoTimeout(10000);
+    doorSocket.send(testPacket);
+    //NOTE: server has to send to receiveMsg.getPort()
+    doorSocket.receive(recievePacket);
+    incorrectPasscodeSuccess = rcvMsg[3]==0;
+    System.out.println("Incorect Passcode Test: ");
+    if(correctPasscodeSuccess) System.out.println("Success");
+    else System.out.println("Failed");
   }
 
 }
